a703a72c635223647f2ea83e1e02307a0548fd24
Muazzam Akhtar <muazzamakhtar@gmail.com>
2024-02-21 15:56:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import cv2
 import numpy as np
 import math
 
-def	face_confidence(face_distance, face_math_threshold=0.6):
-	range = (1.0 - face_math_threshold)
+def	faceConfidence(face_distance, face_match_threshold=0.6):
+	range = (1.0 - face_match_threshold)
 	linear_val = (1.0 - face_distance) / (range * 2.0)
-	
-	if face_distance > face_math_threshold:
+
+	if face_distance > face_match_threshold:
 		return str(round(linear_val * 100, 2)) + '%'
 	else:
-		value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
+		value = (linear_val + ((1.0 - linear_val) * (((linear_val - 0.5) * 2)
+			** 0.2))) * 100
 		return str(round(value, 2)) + '%'
 	
 class FaceRecognition:
@@ -21,23 +22,22 @@ class FaceRecognition:
 	known_face_encodings = []
 	known_face_names = []
 	process_current_frame = True
-	
+
 	def	__init__(self):
 		self.encode_faces()
-		pass
 		# encode faces
-
+	
 	def	encode_faces(self):
 		for image in os.listdir('faces'):
 			face_image = face_recognition.load_image_file(f'faces/{image}')
 			face_encoding = face_recognition.face_encodings(face_image)[0]
 
 			self.known_face_encodings.append(face_encoding)
-			self.known_face_names.append(image)
+			self.known_face_names.append(image.replace('.png', ''))
 		print(self.known_face_names)
 
-	def	run_recognition(self):
-		video_capture = cv2.VideoCapture(0)
+	def	runRecognition(self):
+		video_capture = cv2.VideoCapture(4)
 
 		if not video_capture.isOpened():
 			sys.exit('Video Source not found...')
@@ -47,29 +47,38 @@ class FaceRecognition:
 
 			if self.process_current_frame:
 				small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-				rgb_small_frame =small_frame[:, :, ::-1]
+				rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
+				# rgb_small_frame = small_frame[:, :, ::-1]
 
-				# Find all the faces in the current frame
 				self.face_locations = face_recognition.face_locations(rgb_small_frame)
-				self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+				self.face_encodings = face_recognition.face_encodings(rgb_small_frame,
+					self.face_locations)
 
 				self.face_names = []
 				for face_encoding in self.face_encodings:
-					matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+					matches = face_recognition.compare_faces(self.known_face_encodings,
+						face_encoding)
 					name = 'Unknown'
 					confidence = 'Unknown'
 
-					face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+					face_distances = face_recognition.face_distance(
+						self.known_face_encodings, face_encoding)
 					best_match_index = np.argmin(face_distances)
 					if matches[best_match_index]:
 						name = self.known_face_names[best_match_index]
-						confidence = face_confidence(face_distances[best_match_index])
-
+						confidence = faceConfidence(face_distances[best_match_index])
+						if len(confidence) > 2:
+							_str = confidence[0:3]
+							if float(_str) < 92.0:
+								confidence = 'Unknown'
+								name = 'Unknown'
 					self.face_names.append(f'{name} ({confidence})')
+
 			self.process_current_frame = not self.process_current_frame
 
 			# Display annotations
-			for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+			for (top, right, bottom, left), name in zip(self.face_locations,
+				self.face_names):
 				top *= 4
 				right *= 4
 				bottom *= 4
@@ -78,13 +87,13 @@ class FaceRecognition:
 				cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 				cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), -1)
 				cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+			
 			cv2.imshow('Face Recognition', frame)
-
 			if cv2.waitKey(1) == ord('q'):
 				break
-		video_capture.release()
+
 		cv2.destroyAllWindows()
 
 if __name__ == '__main__':
 	fr = FaceRecognition()
-	fr.run_recognition()
+	fr.runRecognition()
