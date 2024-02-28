dc9ace63260b9aa8fc348e4b7e3ad863f0dd17fa
Muazzam Akhtar <muazzamakhtar@gmail.com>
2024-02-28 12:11:15 +0400

    Ros2 Version Updated

--- main.py
+++ main.py
@@ -15,84 +15,55 @@ def	faceConfidence(face_distance, face_match_threshold=0.6):
 			** 0.2))) * 100
 		return str(round(value, 2)) + '%'
 	
-class FaceRecognition:
-	face_locations = []
-	face_encodings = []
-	face_names = []
-	known_face_encodings = []
-	known_face_names = []
-	process_current_frame = True
-
+class FaceRecognition(object):
 	def	__init__(self):
 		self.encode_faces()
+		self.face_locations = []
+		self.face_encodings = []
+		self.face_names = []
+		self.known_face_encodings = []
+		self.known_face_names = []
+		self.process_current_frame = True
 		# encode faces
 	
 	def	encode_faces(self):
 		for image in os.listdir('faces'):
-			face_image = face_recognition.load_image_file(f'faces/{image}')
-			face_encoding = face_recognition.face_encodings(face_image)[0]
+			self.face_image = face_recognition.load_image_file(f'faces/{image}')
+			self.face_encoding = face_recognition.face_encodings(self.face_image)[0]
 
-			self.known_face_encodings.append(face_encoding)
+			self.known_face_encodings.append(self.face_encoding)
 			self.known_face_names.append(image.replace('.png', ''))
-		print(self.known_face_names)
 
-	def	runRecognition(self):
-		video_capture = cv2.VideoCapture(4)
-
-		if not video_capture.isOpened():
-			sys.exit('Video Source not found...')
-
+	def	runRecognition(self, frame):
 		while True:
-			ret, frame = video_capture.read()
-
-			if self.process_current_frame:
 				small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 				rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
 				# rgb_small_frame = small_frame[:, :, ::-1]
 
-				self.face_locations = face_recognition.face_locations(rgb_small_frame)
-				self.face_encodings = face_recognition.face_encodings(rgb_small_frame,
-					self.face_locations)
+				if self.process_current_frame:
+					self.face_locations = face_recognition.face_locations(rgb_small_frame)
+					self.face_encodings = face_recognition.face_encodings(rgb_small_frame,
+						self.face_locations)
 
-				self.face_names = []
-				for face_encoding in self.face_encodings:
-					matches = face_recognition.compare_faces(self.known_face_encodings,
-						face_encoding)
-					name = 'Unknown'
-					confidence = 'Unknown'
+					for face_encoding in self.face_encodings:
+						matches = face_recognition.compare_faces(self.known_face_encodings,
+							face_encoding)
+						name = 'Unknown'
 
-					face_distances = face_recognition.face_distance(
-						self.known_face_encodings, face_encoding)
-					best_match_index = np.argmin(face_distances)
-					if matches[best_match_index]:
-						name = self.known_face_names[best_match_index]
-						confidence = faceConfidence(face_distances[best_match_index])
-						if len(confidence) > 2:
-							_str = confidence[0:3]
-							if float(_str) < 92.0:
-								confidence = 'Unknown'
-								name = 'Unknown'
-					self.face_names.append(f'{name} ({confidence})')
-
-			self.process_current_frame = not self.process_current_frame
-
-			# Display annotations
-			for (top, right, bottom, left), name in zip(self.face_locations,
-				self.face_names):
-				top *= 4
-				right *= 4
-				bottom *= 4
-				left *= 4
-
-				cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-				cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), -1)
-				cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
-			
-			cv2.imshow('Face Recognition', frame)
-			if cv2.waitKey(1) == ord('q'):
-				break
-
-		cv2.destroyAllWindows()
+						face_distances = face_recognition.face_distance(
+							self.known_face_encodings, face_encoding)
+						best_match_index = np.argmin(face_distances)
+						if matches[best_match_index]:
+							name = self.known_face_names[best_match_index]
+							confidence = faceConfidence(face_distances[best_match_index])
+							if len(confidence) > 2:
+								_str = confidence[0:3]
+								if float(_str) < 92.0:
+									confidence = ''
+									name = 'Unknown'
+									res = False
+						self.face_names.append(name)
+				return self.face_locations, self.face_names
 
 if __name__ == '__main__':
 	fr = FaceRecognition()
